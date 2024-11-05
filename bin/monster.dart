0f028e3ffc93856a605aa2ed1fcc9fcb52d6251f
marion419 <daynvi@gmail.com>
2024-11-05 18:22:43 +0900

    Monster-attackCharacter 작성

--- bin/monster.dart
+++ bin/monster.dart
@@ -7,13 +7,13 @@ class Monster {
   int atk=0;
   int def=0;
 
-  Monster(String this.name, int this.hp, int atk, int characterDef){
+  Monster(String this.name, this.hp, int atk, int characterDef){
     this.atk=max(Random().nextInt(atk+1), characterDef);
     def=0;
   }
 
   void attackCharacter(Character character){
-
+    character.hp-=atk;
   }
 
   void showStatus(){
